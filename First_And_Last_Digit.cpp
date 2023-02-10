48ead23848f5225fa0d7962122a8f056f275b406
coolCicada <1475395755@qq.com>
2023-02-10 22:51:28 +0800

    first and last digit

---
+++ First_And_Last_Digit.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t --) {
+		string a;
+		cin >> a;
+		cout << (a[0] - '0') + (a[a.size() - 1] - '0') << endl;
+	}
+
+	return 0;
+}
