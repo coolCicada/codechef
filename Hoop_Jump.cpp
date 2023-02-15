cd5a0e19894e488fc52e4ede9bee325948f31458
liangsai <liangsai@meituan.com>
2023-02-15 16:23:22 +0800

    Hoop_Jump

---
+++ Hoop_Jump.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t --) {
+		int m;
+		cin >> m;
+
+		cout << m / 2 + 1 << endl;
+
+	}
+
+	return 0;
+}
