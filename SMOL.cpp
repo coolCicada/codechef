4562744d157053cb3698487284b33bee115d6315
liangsai <liangsai@meituan.com>
2023-02-16 11:14:30 +0800

    SMOL

---
+++ SMOL.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t;
+	cin >> t;
+
+	while (t --){
+		int n, k;
+		cin >> n >> k;
+		if (k == 0) cout << n << endl;
+		else cout << n % k << endl;
+	}
+
+	return 0;
+	
+}
